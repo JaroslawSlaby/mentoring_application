c1d37942c118fc48e00f585fd1c42e5d0d84c475
Kaale <jaroslaw.slaby@gmail.com>
2020-04-23 14:31:00 +0200

    test for ssh key

---
+++ src/test/java/pl/js/juniorasks/taskoperations/notifiers/TaskNotifierManagerImplTest.java
@@ -0,0 +1,14 @@
+package pl.js.juniorasks.taskoperations.notifiers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskNotifierManagerImplTest {
+
+    @Test
+    void okTest() {
+
+    }
+
+}
